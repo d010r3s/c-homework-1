8003c54b20c2dbe4bea4af3125bf70ae5560904f
Dolores <104917935+d010r3s@users.noreply.github.com>
2022-12-04 05:28:26 +0300

    format code

--- 1.c
+++ 1.c
@@ -1,27 +1,39 @@
-
 #include <stdio.h>
 #include <math.h>
 #include "tinyexpr.h"
+
+
 typedef double (*TFunc)(te_expr*, double*, double);
 double diff(TFunc f, te_expr *expr, double *_x, double x, double dx);
 double f(te_expr *expr, double *x, double new_x);
 
 int main() {
-double x;
-char c[30];
-te_variable vars[] = {{"x", &x}};
-int err;
-unsigned short i = 1, n;
-scanf("%s",&c);
-te_expr *expr = te_compile(c, vars, 1, &err); 
-printf("Approximation: "); scanf("%lf", &x); printf("Iterations: "); scanf("%d", &n);
-while(i <= n) {
-x = x - te_eval(expr)/diff(f, expr, &x, x, 1e-10);
-++i; }
-printf("Answer: %f \n", x); }
+    int err, n;
+    double x;
+    char c[30];
+    te_variable vars[] = {{"x", &x}};
+
+    printf("Function: ");
+    scanf("%s", &c);
+    printf("Approximation: ");
+    scanf("%lf", &x);
+    printf("Iterations: ");
+    scanf("%d", &n);
+
+    te_expr *expr = te_compile(c, vars, 1, &err);
+
+    for (int i = 0; i < n; ++i) {
+        x = x - te_eval(expr) / diff(f, expr, &x, x, 1e-10);
+    }
+    printf("Answer: %f \n", x);
+}
+
 double diff(TFunc f, te_expr *expr, double *_x, double x, double dx) {
-double dy = f(expr, _x, x+dx)  - f(expr, _x, x-dx);
-return dy/(2.*dx); }
+    double dy = f(expr, _x, x+dx)  - f(expr, _x, x-dx);
+    return dy/(2.*dx);
+}
+
 double f(te_expr *expr, double *x, double new_x) { 
-*x = new_x;
-return te_eval(expr); }
+    *x = new_x;
+    return te_eval(expr);
+}
